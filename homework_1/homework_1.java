4f60e9c400790ee772a3df40a4a1b6b9cdc2c369
REALPROMMS <m.makarov90@bk.ru>
2023-09-05 20:08:19 +0300

    done

--- homework_1/homework_1.java
+++ homework_1/homework_1.java
@@ -1,40 +1,38 @@
-class Answer {
-    public static void arrayOutOfBoundsException() {
-        int[] array = new int[10];
-        System.out.println(array[11]); // Выход за пределы массива
+public class PasswordVerifier {
+    public boolean verify(String password) throws Exception {
+        // Проверка длины пароля
+        if (password.length() < 8) {
+            throw new Exception("Пароль должен быть не менее 8 символов.");
+        }
+
+        // Проверка наличия хотя бы одной цифры
+        if (!password.matches(".*\\d.*")) {
+            throw new Exception("Пароль должен содержать хотя бы одну цифру.");
+        }
+
+        // Проверка наличия хотя бы одной заглавной буквы
+        if (!password.matches(".*[A-Z].*")) {
+            throw new Exception("Пароль должен содержать хотя бы одну заглавную букву.");
+        }
+
+        // Если пароль прошел все проверки, вернуть true
+        return true;
     }
 
-    public static void divisionByZero() {
-        int a = 1;
-        int b = 0;
-        int c = a / b; // Деление на ноль
-    }
-
-    public static void numberFormatException() {
-        String str = "abc";
-        int num = Integer.parseInt(str); // Ошибка преобразования строки в число
-    }
-}
-
-public class Printer {
     public static void main(String[] args) {
-        Answer ans = new Answer();
+        PasswordVerifier verifier = new PasswordVerifier();
+
         try {
-            ans.arrayOutOfBoundsException();
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("Выход за пределы массива");
+            verifier.verify("Weak"); // Этот пароль не соответствует правилам и выбросит исключение
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
 
         try {
-            ans.divisionByZero();
-        } catch (ArithmeticException e) {
-            System.out.println("Деление на ноль");
-        }
-
-        try {
-            ans.numberFormatException();
-        } catch (NumberFormatException e) {
-            System.out.println("Ошибка преобразования строки в число");
+            verifier.verify("StrongPass123"); // Этот пароль соответствует правилам
+            System.out.println("Пароль корректен.");
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
     }
 }
